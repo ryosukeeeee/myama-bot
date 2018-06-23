3d545b1074910185ae6f19ff021c64898fbb28b5
ryoun <ryou.fm.bros@gmail.com>
2018-06-24 08:06:15 +0900

    little fix

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,12 +1,12 @@
-# coding: uft-8
+# coding: utf-8
 
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
 
-# @listen_to('松のや')
-# def listen_func(message):
-#     message.reply('松のやは神')
+@listen_to('松のや')
+def listen_func(message):
+    message.reply('松のやは神')
 
 @listen_to('hello')
 def reply2hello(message):
