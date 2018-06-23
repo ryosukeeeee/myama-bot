a89d494f081055c5ac7812eb34a4fc458b7fbf22
ryoun <ryou.fm.bros@gmail.com>
2018-06-24 08:02:29 +0900

    little fix

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -4,9 +4,9 @@ from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
 
-@listen_to('松のや')
-def listen_func(message):
-    message.reply('松のやは神')
+# @listen_to('松のや')
+# def listen_func(message):
+#     message.reply('松のやは神')
 
 @listen_to('hello')
 def reply2hello(message):
