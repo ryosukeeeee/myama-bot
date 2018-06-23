c64bd525e3aaa8df514c624c4aa7f5a5596f5156
ryoun <ryou.fm.bros@gmail.com>
2018-06-24 07:41:42 +0900

    add response

---
+++ plugins/my_mention.py
@@ -0,0 +1,9 @@
+# coding: uft-8
+
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from slackbot.bot import default_reply
+
+@listen_to('松のや')
+def listen_func(message):
+    message.reply('松のやは神')
