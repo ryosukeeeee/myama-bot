e57b25ec9192bf2623426ac129aa05d96ac11656
ryoun <ryou.fm.bros@gmail.com>
2018-06-25 10:31:04 +0900

    little fix

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -3,6 +3,8 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
+import urllib
+import json
 
 @listen_to('松のや')
 def listen_func(message):
@@ -15,3 +17,35 @@ def reply2hello(message):
 @listen_to('大迫')
 def reply2Osako(message):
 	message.reply('半端ないって')
+	
+@respond_to('今日の天気')
+def weather(message):
+	url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city='
+	# '130010'とすると東京の情報を取得してくれる
+	# ここを変えれば任意の地域の天気情報を取得できる
+	city_id = '080020'
+	html = urllib.request.urlopen(url + city_id)
+	jsonfile = json.loads(html.read().decode('utf-8'))
+	title = jsonfile['title'] 
+	telop = jsonfile['forecasts'][0]['telop']
+	#telopが晴れだったら晴れのスラックのアイコンとか場合分け
+	telop_icon = ''
+	if telop.find('雪') > -1:    
+	    telop_icon = ':showman:'
+	elif telop.find('雷') > -1:
+	    telop_icon = ':thinder_cloud_and_rain:'
+	elif telop.find('晴') > -1:
+	    if telop.find('曇') > -1:
+	        telop_icon = ':partly_sunny:'
+	    elif telop.find('雨') > -1:
+	        telop_icon = ':partly_sunny_rain:'
+	    else:
+	        telop_icon = ':sunny:'
+	elif telop.find('雨') > -1:
+	    telop_icon = ':umbrella:'
+	elif telop.find('曇') > -1:
+	    telop_icon = ':cloud:'
+	else:
+	    telop_icon = ':fire:'
+	text = title + '\n' + '今日の天気　' + telop + telop_icon
+	message.send(text) 
