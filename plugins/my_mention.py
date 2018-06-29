1ba67f1d75ebfaf831e8285d467dd2adf311f000
ryoun <ryou.fm.bros@gmail.com>
2018-06-29 17:13:16 +0900

    make it better

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -96,6 +96,6 @@ def weather(message):
 	description = jsonfile['description']['text']
 	message.send('\n\n【解説】'+'\n'+description)
 
-@respond_to('(名言|めいげん)')
+@respond_to('名言')
 def reply2phrase(message):
 	message.send('空間こそがメディア')
